a50c1c50890eec42bb0345a37bb5db3921da2d98
Anna Gabriella <annagabriellacma@gmail.com>
2021-08-24 10:54:12 -0300

    Adiciona pacientes na tabela

--- js/main.js
+++ js/main.js
@@ -39,3 +39,51 @@ for (let i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+let botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let form = document.querySelector("#form-adiciona");
+
+    let nome = form.nome.value;
+    let peso = form.peso.value;
+    let altura = form.altura.value;
+    let gordura = form.gordura.value;
+
+    let pacienteTr = document.createElement("tr");
+
+    let nomeTd = document.createElement("td");
+    let pesoTd = document.createElement("td");
+    let alturaTd = document.createElement("td");
+    let gorduraTd = document.createElement("td");
+    let imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    /*  imcTd.textContent = imc; */
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    let tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+
+    console.log(pacienteTr);
+    /*   console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+ */
+});
+
+
+titulo.addEventListener("click", function() {
+        console.log("fui clicado");
+    })
+    /* console.log("Oi eu sou o botao e fui clicado"); */
